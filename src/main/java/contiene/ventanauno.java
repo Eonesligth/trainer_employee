97138431f987ae5fb9476a69c1e0b3344ba08e22
eonesligth <eonesligth@hotmail.com>
2023-10-06 10:56:00 -0500

    files app

---
+++ src/main/java/contiene/ventanauno.java
@@ -0,0 +1,10 @@
+
+package contiene;
+
+public class ventanauno {
+    public static void main(String[] args) {
+        introventana miInicio=new introventana () ;
+        miInicio.setVisible(true);
+    }
+    
+}
